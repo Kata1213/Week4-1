d3c1d796b0d636297cf39ab7e528e68662e50152
Kata <2726456039@qq.com>
2018-04-13 11:11:02 +0800

    modified:   src/main/java/com/tw/Library.java 	modified:   src/test/java/com/tw/LibraryTest.java

--- src/main/java/com/tw/Library.java
+++ src/main/java/com/tw/Library.java
@@ -1,7 +1,165 @@
 package com.tw;
 
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class Library {
-    public boolean someLibraryMethod() {
+
+    private static List<String> course = new ArrayList<>();
+    static List<StudentInfo> report = new ArrayList<>();
+
+    private static final Pattern patternName = Pattern.compile("^[\\u4E00-\\u9FA5A-Za-z]+$");
+    private static final Pattern patternNumber = Pattern.compile("^[0-9]+$");
+    private static final Pattern patternGrade = Pattern.compile("^(\\d{1}|[1-9]\\d{1}|1[0-1]\\d{1}|100)$");
+
+    private static final DecimalFormat decimalFormat = new DecimalFormat("###################.##");
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        printMenu("menu");
+        while (sc.hasNextLine()) {
+            String inputStr = sc.nextLine().trim();
+            List<String> inputList;
+            if (inputStr.equals("3")) {
+                break;
+            }
+            switch (inputStr) {
+                case "1":
+                    printMenu("add");
+                    while (sc.hasNextLine()) {
+                        String input = sc.nextLine();
+                        inputList = Arrays.asList(input.split(","));
+                        if (addStudentInfo(inputList)) {
+                            break;
+                        }
+                        printMenu("falseAdd");
+                    }
+                    printMenu("menu");
+                    break;
+                case "2":
+                    printMenu("output");
+                    inputList = new ArrayList<>();
+                    while (sc.hasNextLine()) {
+                        String input = sc.nextLine();
+                        inputList.clear();
+                        inputList.addAll(Arrays.asList(input.split(",")));
+                        if (generateGrades(inputList)) {
+                            break;
+                        }
+                        printMenu("falseOutput");
+                    }
+                    printMenu("menu");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    //打印目录信息
+    private static void printMenu(String result) {
+        switch (result) {
+            case "menu":
+                System.out.print("1. 添加学生\n" +
+                        "2. 生成成绩单\n" +
+                        "3. 退出\n" +
+                        "请输入你的选择(1~3):\n");
+                break;
+            case "add":
+                System.out.print("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n");
+                break;
+            case "output":
+                System.out.print("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+                break;
+            case "falseAdd":
+                System.out.print("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n");
+                break;
+            case "falseOutput":
+                System.out.print("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public static boolean addStudentInfo(List<String> inputList) {
+        if (!judgeStudentInfo(inputList)) {
+            return false;
+        }
+        Map<String, Integer> grades = new HashMap<>();
+        StudentInfo student = new StudentInfo();
+        student.setName(inputList.get(0));
+        student.setNumber(inputList.get(1));
+        inputList.stream().skip(2).forEach(n -> {
+            String[] item = n.split(":");
+            grades.put(item[0].trim(), Integer.parseInt(item[1].trim()));
+        });
+        student.setGrades(grades);
+        report.add(student);
+        System.out.print("学生" + student.getName() + "的成绩被添加\n");
         return true;
     }
+    private static boolean judgeStudentInfo(List<String> inputList) {
+        if (inputList.size() != 6) {
+            return false;
+        }
+        if (!judgePattern(inputList.get(0), patternName)) return false;
+        if (!judgePattern(inputList.get(1), patternNumber)) return false;
+        boolean flag = true;
+        for (int i = 2; i < inputList.size(); i++) {
+            String[] item = inputList.get(i).split(":");
+            course.add("数学");
+            course.add("语文");
+            course.add("英语");
+            course.add("编程");
+            if (!course.contains(item[0])
+                    || !judgePattern(item[1], patternGrade)) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public static boolean generateGrades(List<String> inputList) {
+        boolean flag = inputList.stream().allMatch(n -> judgePattern(n, patternNumber));
+        if (!flag) {
+            return false;
+        }
+        double classAvg = report.stream().mapToInt(StudentInfo::getTotal).average().orElse(0);
+        double classMid = 0;
+        int size = report.size();
+        if (size != 0) {
+            if (size % 2 == 0) {
+                classMid = report.stream().sorted(Comparator.comparing(StudentInfo::getTotal)).skip(size / 2 - 1).limit(2).mapToInt(StudentInfo::getTotal).average().getAsDouble();
+            } else {
+                classMid = report.stream().sorted(Comparator.comparing(StudentInfo::getTotal)).skip(size / 2).limit(1).mapToInt(StudentInfo::getTotal).sum();
+            }
+        }
+
+        System.out.print("成绩单\n");
+        System.out.print("姓名|数学|语文|英语|编程|平均分|总分\n");
+        System.out.print("========================\n");
+        report.stream().filter(n -> inputList.contains(Integer.toString(n.getNumber()))).forEach(
+                n -> System.out.print(n.getName() + "|"
+                        + n.getGrades().get("数学") + "|"
+                        + n.getGrades().get("语文") + "|"
+                        + n.getGrades().get("英语") + "|"
+                        + n.getGrades().get("编程") + "|"
+                        + decimalFormat.format(n.getAverage()) + "|"
+                        + n.getTotal() + "\n"));
+        System.out.print("========================\n");
+        System.out.print("全班总分平均数：" + decimalFormat.format(classAvg) + "\n");
+        System.out.print("全班总分中位数：" + decimalFormat.format(classMid) + "\n");
+
+        return true;
+    }
+
+    private static boolean judgePattern(String input, Pattern patternName) {
+        return patternName.matcher(input).matches();
+    }
+
 }
--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,7 +1,10 @@
 package com.tw;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import static org.hamcrest.core.Is.is;
@@ -11,30 +14,45 @@ import static org.mockito.Mockito.when;
 import java.io.ByteArrayOutputStream;
 
 public class LibraryTest {
+    private ByteArrayOutputStream outContent;
+
+    @Before
+    public void setUp() throws Exception {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Library.report.clear();
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
     @Test
     public void testAddScore() {
-        String s1 = "张三,000001,数学:60,语文:60,英语:60,编程:60";
-        String should_result="学生张三的成绩被添加";
-        String real_result=new Library().toString();
-        assertEquals(should_result, real_result);
+        String s1 = "张三,0001,数学:60,语文:60,英语:60,编程:60";
+        assertTrue(Library.addStudentInfo(Arrays.asList(s1.split(","))));
+        assertTrue(systemOut().endsWith("学生张三的成绩被添加\n"));
 
     }
     @Test
     public void testPtrintScore() {
-        String s1 = "张三,000001,数学:60,语文:60,英语:60,编程:60";
-        String s2 = "李四,000002,数学:90,语文:90,英语:90,编程:90";
-        String should_result="学生张三的成绩被添加\n" +
+        String s1 = "张三,0001,数学:60,语文:60,英语:60,编程:60";
+        String s2 = "李四,0002,数学:90,语文:90,英语:90,编程:90";
+        assertTrue(Library.addStudentInfo(Arrays.asList(s1.split(","))));
+        assertTrue(Library.addStudentInfo(Arrays.asList(s2.split(","))));
+        String s3 = "0001,0002";
+        assertTrue(Library.generateGrades(Arrays.asList(s3.split(","))));
+        assertEquals("学生张三的成绩被添加\n" +
                 "学生李四的成绩被添加\n" +
                 "成绩单\n" +
                 "姓名|数学|语文|英语|编程|平均分|总分\n" +
                 "========================\n" +
                 "张三|60|60|60|60|60|300\n" +
-                "李四|95|90|90|90|90|450\n" +
+                "李四|90|90|90|90|90|360\n" +
                 "========================\n" +
-                "全班总分平均数：375\n" +
-                "全班总分中位数：375\n";
-        String real_result=new Library().toString();
-        assertEquals(should_result, real_result);
+                "全班总分平均数：330\n" +
+                "全班总分中位数：330\n", systemOut());
+    }
 
     }
 
@@ -58,4 +76,4 @@ public class LibraryTest {
 
     }*/
 
-}
+
